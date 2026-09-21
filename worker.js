eec94e1f50a46e326bb604e79186986b89f0f90e
217heidai <217heidai@gmail.com>
2026-09-21 17:26:45 +0800

    更新白名单

--- worker.js
+++ worker.js
@@ -27,10 +27,9 @@ addEventListener('fetch', event => {
 const PROXY_CONFIG = {
   enableWhiteList: true,
   whiteListUrls: [
-    "https://github.com/217heidai/*",
-    "https://raw.githubusercontent.com/217heidai/*",
-    "https://raw.githubusercontent.com/Lin-arm/GKD_subscription/*",
-    "https://raw.githubusercontent.com/suxuang/myIPTV/*",
+    "https://github.com/*",
+    "https://raw.githubusercontent.com/*",
+    "https://release-assets.githubusercontent.com/*",
     "https://api.telegram.org/*",
     "https://prod.ave-api.com/*",
     "https://www.binance.com/api/*",
